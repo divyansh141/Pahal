17ff62b7a93a83d75b8fba73b8897d59275765fc
divyansh141 <64478348+divyansh141@users.noreply.github.com>
2021-05-07 14:22:08 +0530

    Add files via upload

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,105 +1,70 @@
-window.addEventListener('load', function() {
-    var form = document.getElementById('form');
-    var individual = form;
-    resultHTML = '';
-        function orgForm(){
-                resultHTML = '<form action="" class="login-form">\
-                <!-- Organization name-->\
-                <div class="input-group">\
-                  <input type="text" placeholder="Organization name" id="name">\
-                </div>\
-                <!-- Username or Email -->\
-                <div class="input-group">\
-                  <input type="email" placeholder="Username or Email" id="username">\
-                </div>\
-                <div class="input-group">\
-                    <input type="tel" placeholder="Mobile number" id="number" pattern="[0-9]{10}">\
-                </div>\
-                <!-- City-->\
-                <div class="input-group">\
-                  <input type="text" placeholder="Current working city" id="city">\
-                </div>\
-                <!-- State-->\
-                <div class="input-group">\
-                  <input type="text" placeholder="State" id="state">\
-                </div>\
-                <!-- Address -->\
-                <div class="input-group">\
-                  <input type="text" placeholder="Permanent Address" id="address">\
-                </div>'
-                
-            form.innerHTML=resultHTML;
-        }
+(function ($) {
+    'use strict';
+    /*==================================================================
+        [ Daterangepicker ]*/
+    try {
+        $('.js-datepicker').daterangepicker({
+            "singleDatePicker": true,
+            "showDropdowns": true,
+            "autoUpdateInput": false,
+            locale: {
+                format: 'DD/MM/YYYY'
+            },
+        });
+    
+        var myCalendar = $('.js-datepicker');
+        var isClick = 0;
+    
+        $(window).on('click',function(){
+            isClick = 0;
+        });
+    
+        $(myCalendar).on('apply.daterangepicker',function(ev, picker){
+            isClick = 0;
+            $(this).val(picker.startDate.format('DD/MM/YYYY'));
+    
+        });
+    
+        $('.js-btn-calendar').on('click',function(e){
+            e.stopPropagation();
+    
+            if(isClick === 1) isClick = 0;
+            else if(isClick === 0) isClick = 1;
+    
+            if (isClick === 1) {
+                myCalendar.focus();
+            }
+        });
+    
+        $(myCalendar).on('click',function(e){
+            e.stopPropagation();
+            isClick = 1;
+        });
+    
+        $('.daterangepicker').on('click',function(e){
+            e.stopPropagation();
+        });
+    
+    
+    } catch(er) {console.log(er);}
+    /*[ Select 2 Config ]
+        ===========================================================*/
+    
+    try {
+        var selectSimple = $('.js-select-simple');
+    
+        selectSimple.each(function () {
+            var that = $(this);
+            var selectBox = that.find('select');
+            var selectDropdown = that.find('.select-dropdown');
+            selectBox.select2({
+                dropdownParent: selectDropdown
+            });
+        });
+    
+    } catch (err) {
+        console.log(err);
+    }
+    
 
-        function indiForm (){
-                resultHTML = '<form action="" class="login-form">\
-                <!-- Full name-->\
-                <div class="input-group">\
-                  <input type="text" placeholder="Full name" id="name">\
-                </div>\
-                <!-- Username or Email -->\
-                <div class="input-group">\
-                  <input type="email" placeholder="Username or Email" id="username">\
-                </div>\
-                <div class="input-group">\
-                    <input type="tel" placeholder="Mobile number" id="number" pattern="[0-9]{10}">\
-                </div>\
-                <!-- DOB -->\
-                <div class="input-group">\
-                  <input type="date" placeholder="Date of birth" id="dob">\
-                </div>\
-                <!--Gender-->\
-                <div class="input-group">\
-                    <p style="margin-bottom: 0; align-self: flex-start;">What\'s your gender?</p>\
-                    <div style="align-self: flex-start;">\
-                    <input type="radio" id="female" name="gender" value="female">\
-                    <label for="female">Female</label>\
-                    <input type="radio" id="other" name="gender" value="other">\
-                    <label for="other">Rather not to say</label>\
-                    </div>\
-                </div>\
-                <!-- City-->\
-                <div class="input-group">\
-                  <input type="text" placeholder="Current city" id="city">\
-                </div>\
-                <!-- State-->\
-                <div class="input-group">\
-                  <input type="text" placeholder="State" id="state">\
-                </div>\
-                <!-- Address -->\
-                <div class="input-group">\
-                  <input type="text" placeholder="Permanent Address" id="address">\
-                </div>\
-                <!--Martial Status -->\
-                <div class="input-group">\
-                    <p style="margin-bottom: 0; align-self: flex-start;">What\'s your martial status?</p>\
-                    <div style="align-self: flex-start;">\
-                    <input type="radio" id="single" name="martial" value="single">\
-                    <label for="single">Single</label>\
-                    <input type="radio" id="married" name="martial" value="married">\
-                    <label for="married">Married</label>\
-                    <input type="radio" id="divorced" name="martial" value="divorced">\
-                    <label for="divorced">Divorced</label>\
-                    </div>\
-                </div>\
-                <!--Language Known -->\
-                <div class="input-group">\
-                    <p style="margin-bottom: 0; align-self: flex-start;">What\'s language you speak?</p>\
-                    <div>\
-                    <input type="checkbox" id="hindi" name="language" value="hindi">\
-                    <label for="hindi">Hindi</label>\
-                    <input type="checkbox" id="english" name="language" value="english">\
-                    <label for="english">English</label>\
-                    <input type="checkbox" id="other" name="language" value="other">\
-                    <label for="other">Other local language</label>\
-                    </div>\
-                </div>'
-
-                form.innerHTML=resultHTML;
-        }
-        var org = document.getElementById('org');
-        org.addEventListener('click', orgForm);
-
-        var indi = document.getElementById('individual');
-        indi.addEventListener('click', indiForm);
-    });
+})(jQuery);
